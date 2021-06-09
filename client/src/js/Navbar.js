2e1032884fac81256fb256e217481a9d47d6583e
Diego Marino <marinodiego.96@gmail.com>
2021-06-09 18:33:54 +0200

    add navbar component

---
+++ client/src/js/Navbar.js
@@ -0,0 +1,26 @@
+import { Navbar, Nav, Button } from "react-bootstrap/";
+import { PersonFill, JournalCheck } from "react-bootstrap-icons";
+import "../css/Navbar.css";
+
+const MyNavbar = (props) => {
+  return (
+    <Navbar id='navbar' variant='light' fixed='top' className='px-3 d-flex justify-content-between'>
+      {/* <Navbar.Toggle aria-controls="left-sidebar" onClick={this.showSidebar}/> */}
+      <Navbar.Toggle aria-controls='left-sidebar' />
+      <Navbar.Brand href='/'>
+        <JournalCheck className='mr-1' size='30' /> Survey.me
+      </Navbar.Brand>
+      <Nav className='ml-auto'>
+        <Nav.Item>
+          <Nav.Link href='#'>
+            <Button id='btn-login' variant='primary' className='px-3'>
+              <PersonFill size='20' className='m-1' /> Login as Admin
+            </Button>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
