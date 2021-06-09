5d1a784f1f7849bf1ee55d752ec711f6b202c053
Diego Marino <marinodiego.96@gmail.com>
2021-06-10 00:06:33 +0200

    add default, survey routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import { React } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, useParams, useHistory, Switch, Redirect } from "react-router-dom";
 import { Container, Row, Col, Button } from "react-bootstrap/";
-import MyNavbar from "./js/Navbar";
-import SurveyCard from "./js/SurveyCard";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import MyNavbar from "./components/Navbar";
+import SurveyCard from "./components/SurveyCard";
+import Survey from "./components/Survey";
 
 const cards = [1, 2, 3, 4, 5];
 function App() {
@@ -11,13 +13,26 @@ function App() {
     <Container fluid>
       <MyNavbar />
       <Row className='vh-100 below-nav'>
-        <Col className='text-center'>
-          <h3 id='title-main' className='my-4 text-center text-uppercase'>
-            Available surveys
-          </h3>
-          {cards.map((n) => (
-            <SurveyCard key={n.toString()} />
-          ))}
+        <Col className='text-center p-0'>
+          <Router>
+            <Switch>
+              <Route exact path='/'>
+                <h3 id='title-main' className='my-4 text-center text-uppercase'>
+                  Available surveys
+                </h3>
+                {cards.map((n) => (
+                  <SurveyCard key={n.toString()} />
+                ))}
+              </Route>
+              <Route path={["/survey/:id"]}>
+                <h3 id='title-main' className='my-4 text-center'>
+                  Survey Title
+                </h3>
+                <Survey />
+              </Route>
+              <Redirect to='/' />
+            </Switch>
+          </Router>
         </Col>
       </Row>
     </Container>
