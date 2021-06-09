a5996591dca9ca7da911fdfcc6f9808db247be43
Diego Marino <marinodiego.96@gmail.com>
2021-06-09 22:49:34 +0200

    mock SurveyCards

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,24 @@ import { React } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Button } from "react-bootstrap/";
 import MyNavbar from "./js/Navbar";
+import SurveyCard from "./js/SurveyCard";
 import "./App.css";
 
+const cards = [1, 2, 3, 4, 5];
 function App() {
   return (
     <Container fluid>
       <MyNavbar />
-      <Row className='vh-100' />
+      <Row className='vh-100 below-nav'>
+        <Col className='text-center'>
+          <h3 id='title-main' className='my-4 text-center text-uppercase'>
+            Available surveys
+          </h3>
+          {cards.map((n) => (
+            <SurveyCard key={n.toString()} />
+          ))}
+        </Col>
+      </Row>
     </Container>
   );
 }
