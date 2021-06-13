8fd0cb092a9b99ee4d27d6bfdef5429e1ae5ece4
Diego Marino <marinodiego.96@gmail.com>
2021-06-13 23:03:49 +0200

    use uuid as key for questions objects

--- client/src/components/SurveyTemplate.js
+++ client/src/components/SurveyTemplate.js
@@ -3,22 +3,33 @@ import { PencilSquare } from "react-bootstrap-icons";
 import "../css/Survey.css";
 import QuestionTemplate from "./question_template/QuestionTemplate";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const questionTemplate = {id: 0, min: 0, max: 0, text: "Type your question here", answers: []};
+const questionTemplate = { key: uuidv4(), id: 0, min: 0, max: 0, text: "Type your question here", answers: [] };
 
-function SurveyTemplate (props) {
+function SurveyTemplate(props) {
   const [questions, setQuestions] = useState([questionTemplate]);
 
   const handleAddQuestion = () => {
-    setQuestions([...questions, { id: questions.length, min: 0, max: 0, text: "Type your question here", answers: [] }]);
+    setQuestions([
+      ...questions,
+      {
+        key: uuidv4(),
+        id: questions.length,
+        min: 0,
+        max: 0,
+        text: "Type your question here",
+        answers: [],
+      },
+    ]);
   };
 
   return (
     <div className='survey-page'>
       <div className='mx-5'>
-          {questions.map((q) => (
-          <QuestionTemplate key={q.id} questionList={questions} setQuestionList={setQuestions} question={q} />
-          ))}
+        {questions.map((q) => (
+          <QuestionTemplate key={q.key} questionList={questions} setQuestionList={setQuestions} question={q} />
+        ))}
         <Button variant='primary' block className='mb-5 mt-5' onClick={handleAddQuestion}>
           Add a question
         </Button>
@@ -28,6 +39,6 @@ function SurveyTemplate (props) {
       </div>
     </div>
   );
-};
+}
 
 export default SurveyTemplate;
