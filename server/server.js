037fff65569a97498361411bfa0878ed0d120eb8
Diego Marino <marinodiego.96@gmail.com>
2021-06-16 01:04:08 +0200

    setup missing middlewares

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ const { check, validationResult } = require("express-validator"); // validation 
 const app = new express();
 const port = 3001;
 
+// set-up the middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
 // activate the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
