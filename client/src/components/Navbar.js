a8d2c5cfa26bfbc2c5478981622ee199af803902
Diego Marino <marinodiego.96@gmail.com>
2021-06-20 11:33:23 +0200

    new navbar style

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,10 +9,8 @@ const MyNavbar = (props) => {
     <Navbar id='navbar' variant='light' fixed='top' className='px-3 d-flex flex-row justify-content-between'>
       {/* <Navbar.Toggle aria-controls="left-sidebar" onClick={this.showSidebar}/> */}
       <Navbar.Toggle aria-controls='left-sidebar' />
-      <Link id='nav-home' to='/'>
-        <Navbar.Brand id='navbar-brand'>
-          <JournalCheck className='mr-1' size='30' /> Survey.me
-        </Navbar.Brand>
+      <Link id='nav-home' to={loggedIn ? "/admin/mySurveys" : "/"}>
+        <Navbar.Brand id='navbar-brand'>SurvHey!</Navbar.Brand>
       </Link>
       <Nav className='ml-auto'>
         <Nav.Item>
@@ -21,7 +19,7 @@ const MyNavbar = (props) => {
               <h5 id='nav-username' className='mx-4 my-0'>
                 Hi, {user.username}
               </h5>
-              <Button id='btn-login' variant='primary' onClick={logout}>
+              <Button id='btn-login' variant='outline-primary' onClick={logout}>
                 <BoxArrowLeft size='20' className='m-1' /> Logout
               </Button>
             </div>
