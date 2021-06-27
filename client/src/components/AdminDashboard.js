8d5c75659d7d9d4762c446d8ef78fde95d972cb9
Diego Marino <marinodiego.96@gmail.com>
2021-06-27 23:21:30 +0200

    AdminDashboard: add feedback messages

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -66,32 +66,37 @@ function AdminDashboard(props) {
         </ListGroup>
         <Col sm={8} id='carousel-col' className='p-0 m-0'>
           {selectedSId != "" ? (
-            <Carousel
-              className=''
-              activeIndex={index}
-              onSelect={handleSelect}
-              interval={null}
-              controls={true}
-              nextIcon={
-                <span>
-                  <ChevronRight size={30} className='carousel-ic' />
-                </span>
-              }
-              prevIcon={
-                <span>
-                  <ChevronLeft size={30} className='carousel-ic' />
-                </span>
-              }>
-              {/*TODO map submissions */}
-              {submissions
-                .filter((s) => s.surveyId === selectedSId)
-                .map((s, index) => (
-                  <Carousel.Item key={index}>
-                    <Survey readOnly={true} sId={selectedSId} sub={s} />
-                  </Carousel.Item>
-                ))}
-            </Carousel>
-          ) : null}
+            submissions.filter((it) => it.surveyId === selectedSId).length > 0 ? (
+              <Carousel
+                className=''
+                activeIndex={index}
+                onSelect={handleSelect}
+                interval={null}
+                controls={true}
+                nextIcon={
+                  <span>
+                    <ChevronRight size={30} className='carousel-ic' />
+                  </span>
+                }
+                prevIcon={
+                  <span>
+                    <ChevronLeft size={30} className='carousel-ic' />
+                  </span>
+                }>
+                {submissions
+                  .filter((s) => s.surveyId === selectedSId)
+                  .map((s, index) => (
+                    <Carousel.Item key={index}>
+                      <Survey readOnly={true} sId={selectedSId} sub={s} />
+                    </Carousel.Item>
+                  ))}
+              </Carousel>
+            ) : (
+              <h4 className='mt-3'>No submissions yet.</h4>
+            )
+          ) : (
+            <h4 className='mt-3'>Select a survey on the left to view the submissions.</h4>
+          )}
         </Col>
         <Link to='/admin/create'>
           <Fab />
